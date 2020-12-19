4d33e89a6e2a53f2ac486eaf74f68adf37d7129a
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2020-12-18 16:20:54 -0800

    finishing for the day

---
+++ playground/callbacks.js
@@ -0,0 +1,23 @@
+// const geocode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = {
+//       latitude: 0,
+//       longtitude: 0,
+//     };
+//     callback(data);
+//   }, 2000);
+// };
+// geocode("Philadelphia", (data) => {
+//   console.log(data);
+// });
+
+const add = (num, num1, callback) => {
+setTimeout(() => {
+callback(num + num1)
+}, 2000)
+}
+
+
+add(1, 4, (sum) => {
+    console.log(sum)
+})
